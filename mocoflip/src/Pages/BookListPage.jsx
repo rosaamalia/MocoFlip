874b85982a7748fe5560dc7023f2c2ab84e1d771
rosaamalia <amaliarosaaa15@gmail.com>
2021-11-19 09:14:31 +0700

    menambahkan filter kategori

--- mocoflip/src/Pages/BookListPage.jsx
+++ mocoflip/src/Pages/BookListPage.jsx
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from "react";
-import { Container, Row, Button } from "react-bootstrap";
+import { Container, Row, Dropdown, DropdownButton } from "react-bootstrap";
 import styled from 'styled-components';
 import BookItem from '../components/BookList/BookItem';
 import BlockLoading from '../components/Loading/BlockLoading';
@@ -7,9 +7,12 @@ import BlockLoading from '../components/Loading/BlockLoading';
 function BookListPage() {
     const [loading, setLoading] = useState(true);
     const [books, setBooks] = useState([]);
+    const [scienceBooks, setScienceBooks] = useState([]);
+    const [technologyBooks, setTechnologyBooks] = useState([]);
+    const [astronomyBooks, setAstronomyBooks] = useState([]);
     
     useEffect(() => {
-       fetch('https://books.googleapis.com/books/v1/volumes?q=physics&filter=free-ebooks')
+       fetch('https://books.googleapis.com/books/v1/volumes?q=education&filter=free-ebooks')
         .then(res => {
             return res.json();
         })
@@ -19,7 +22,38 @@ function BookListPage() {
         })
     }, []);
 
-    let bookItems = books.items;
+    useEffect(() => {
+        fetch('https://books.googleapis.com/books/v1/volumes?q=science&filter=free-ebooks')
+         .then(res => {
+             return res.json();
+         })
+         .then(data => {
+             setScienceBooks(data);
+             setLoading(false);
+         })
+     }, []);
+    
+     useEffect(() => {
+        fetch('https://books.googleapis.com/books/v1/volumes?q=technology&filter=free-ebooks')
+         .then(res => {
+             return res.json();
+         })
+         .then(data => {
+             setTechnologyBooks(data);
+             setLoading(false);
+         })
+     }, []);
+
+     useEffect(() => {
+        fetch('https://books.googleapis.com/books/v1/volumes?q=astronomy&filter=free-ebooks')
+         .then(res => {
+             return res.json();
+         })
+         .then(data => {
+             setAstronomyBooks(data);
+             setLoading(false);
+         })
+     }, []);
     
     if(loading) {
         return (
@@ -29,18 +63,37 @@ function BookListPage() {
         )
     }
 
+    if(books == []) {
+        return (
+            <Container fluid className="d-flex flex-wrap justify-content-center min-vh-100" style={{ backgroundColor: '#1B232E', alignItems: 'center' }}>
+                <p>Error to fetch data</p>
+            </Container>
+        )
+    }
+
     return (
         <Container fluid className="d-flex flex-wrap justify-content-center p-5" style={{ backgroundColor: '#1B232E' }}>
                 <Row style={{ color: 'white', textAlign: 'center'}}>
                     <p className="text-center fs-2 fw-bold py-0 my-0">Book List</p>
-                    <p className="text-center" style={{ marginBottom: 30 }}>Find your favorite book!</p>
+                    <p className="text-center" style={{ marginBottom: 20 }}>Find your favorite book!</p>
+                    <Dropdown style={{ marginBottom: 30}}>
+                        <Dropdown.Toggle variant="secondary" style={{ backgroundColor: '#BE428D', borderStyle: 'none' }}>
+                            Choose Category
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu variant="dark">
+                            <Dropdown.Item onClick={() => {setBooks(scienceBooks)}} value="Science">Science</Dropdown.Item>
+                            <Dropdown.Item onClick={() => {setBooks(technologyBooks)}} value="Technology">Technology</Dropdown.Item>
+                            <Dropdown.Item onClick={() => {setBooks(astronomyBooks)}} value="Astronomy">Astronomy</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </Row>
                 <Row className="justify-content-center gap-5 p-0">
-                    {bookItems.map(function(book, i) {
+                    {books.items ? books.items.map(function(book) {
                         return (
                             <BookItem bookItem={book}/>
                         )
-                    })}
+                    }) : setLoading(true) }
                 </Row>
         </Container>   
     )
--- mocoflip/src/Pages/ReadPage.jsx
+++ mocoflip/src/Pages/ReadPage.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import {Button, Col, Container, Form, Row, Card, CardImg} from "react-bootstrap";
 
 function LoginPage() {
-
+    let url = "http://play.google.com/books/reader?id=DZ7cZags_xwC&hl=&as_brr=7&printsec=frontcover&source=gbs_api";
     return (
         <div className="d-flex align-items-center min-vh-100" style ={{ backgroundColor: '#1B232E'}}>
         <Container fluid>
-            
+            <iframe src={url} title="Iframe Example" style={{ height: 500}}></iframe>
         </Container>  
         </div>
     );
