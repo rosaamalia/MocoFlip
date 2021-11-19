43c99c28ad98fbbe609f9c428c673f3251d13b53
rosaamalia <amaliarosaaa15@gmail.com>
2021-11-19 17:44:46 +0700

    edit gambar

--- mocoflip/src/components/BookViewer/BookViewer.jsx
+++ mocoflip/src/components/BookViewer/BookViewer.jsx
@@ -7,51 +7,49 @@ const ViewerCanvas = styled.div`
     height: 600px;
 `;
 
-function BookViewer(props) { 
-    let id = props.bookId;
-    const canvasRef = useRef()     
-    
-    const [loaded, setLoaded] = useState(false);  
+function BookViewer(props){    
+  const canvasRef = useRef()     
+  
+  const [loaded, setLoaded] = useState(false);  
 
-    function alertNotFound() {            
-        alert("could not embed the book!");
-     }
+  function alertNotFound() {            
+      alert("could not embed the book!");
+   }
 
-    useEffect(()=> {      
-       const scriptTag = document.createElement('script')                
-       scriptTag.src= 'https://www.google.com/books/jsapi.js'       
-       scriptTag.addEventListener('load', ()=>setLoaded(true))       
-       scriptTag.id = "google-script"      
-       document.body.appendChild(scriptTag);       
-     }, []);      
+  useEffect(()=> {      
+     const scriptTag = document.createElement('script')                
+     scriptTag.src= 'https://www.google.com/books/jsapi.js'       
+     scriptTag.addEventListener('load', ()=>setLoaded(true))       
+     scriptTag.id = "google-script"      
+     document.body.appendChild(scriptTag);       
+   }, []);      
 
-    useEffect(()=>{            
-        if (!loaded) return             
-        else{         
-             if(window.viewer){            
-                let viewer = new window.google.books.DefaultViewer
-                (canvasRef.current); 
-                //pake id
-                viewer.load(props.bookId, alertNotFound);                    
-              }        
-              else{          
-                window.google.books.load()                             
-                window.google.books.setOnLoadCallback(() => {                 
-                let viewer = new window.google.books.DefaultViewer      
-                    (canvasRef.current);         
-                window.viewer = viewer;         
-                viewer.load(props.bookId, alertNotFound);        
-              })
-            }              
-        }}, [loaded])      
-        return (      
-           <div>        
-               {loaded ?             
-                    <div>                
-                       <ViewerCanvas ref={canvasRef}></ViewerCanvas>            
-                    </div> : 
-               'Script not loaded'}      
-           </div>    )
- };
+  useEffect(()=>{            
+      if (!loaded) return             
+      else{         
+           if(window.viewer){            
+              let viewer = new window.google.books.DefaultViewer
+              (canvasRef.current); 
+              viewer.load(props.bookId, alertNotFound);                    
+            }        
+            else{          
+              window.google.books.load()                             
+              window.google.books.setOnLoadCallback(() => {                 
+              let viewer = new window.google.books.DefaultViewer      
+                  (canvasRef.current);         
+              window.viewer = viewer         
+              viewer.load(props.bookId, alertNotFound);        
+            })
+          }              
+      }}, [loaded])      
+      return (      
+         <div>        
+             {loaded ?             
+                  <div>                
+                     <ViewerCanvas ref={canvasRef} id="viewerCanvas"></ViewerCanvas>            
+                  </div> : 
+             'Script not loaded'}      
+         </div>    );
+};
 
  export default BookViewer;
